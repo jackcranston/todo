f6a5a3c0d9a8089fd39317775362899d4a4d5c17
Jack Cranston <jackcranston@itg.co.uk>
2021-04-12 10:04:23 +0100

    Added edit logic and some basic styling

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -3,25 +3,43 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheck, faTimes, faPen } from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faTimes, faPen, faSave } from '@fortawesome/free-solid-svg-icons';
 
 import './index.scss';
 
 const Card = (props) => {
   const { id, title, completed, updateTodo, removeTodo, completeTodo } = props;
-  const [inputValue, setValue] = useState(title);
+  const [state, setState] = useState({
+    inputValue: title,
+    editing: false
+  });
 
   const handleInputChange = (event) => {
-    setValue(event.target.value);
+    setState({
+      ...state,
+      inputValue: event.target.value
+    });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    updateTodo(id, inputValue);
+    updateTodo(id, state.inputValue);
+    setState({
+      ...state,
+      editing: !state.editing
+    })
   };
+
+  const toggleEditable = () => {
+    setState({
+      ...state,
+      editing: !state.editing
+    })
+  }
 
   const classes = [
     'card',
+    !state.editing ? '' : 'card--editing',
     !completed ? '' : 'card--complete'
   ]
 
@@ -29,20 +47,23 @@ const Card = (props) => {
     <div className={classes.join(' ')} id={`card-${id}`} data-testid="card">
       <form className="card__form" data-testid="card-form" onSubmit={handleSubmit}>
         <label className="card__label" htmlFor={`card-input-${id}`} data-testid="card-label">
-          {title}
-          <input className="card__input" id={`card-input-${id}`} value={inputValue} onChange={handleInputChange} data-testid="card-input" />
-          <button className="card__submit" data-testid="card-submit" type="submit">Save changes</button>
+          <span className="card__title">{title}</span>
+          <input className="card__input" id={`card-input-${id}`} value={state.inputValue} onChange={handleInputChange} data-testid="card-input" />
         </label>
+        <button className="card__button card__button--submit" data-testid="card-submit" type="submit">
+          <span className="sr-only">Save</span>
+          <FontAwesomeIcon icon={faSave} />
+        </button>
       </form>
-      <button className="card__button" type="button" data-testid="card-edit">
+      <button className="card__button card__button--edit" type="button" data-testid="card-edit" onClick={toggleEditable}>
         <span className="sr-only">Edit</span>
         <FontAwesomeIcon icon={faPen} />
       </button>
-      <button className="card__button" type="button" onClick={() => removeTodo(id)} data-testid="card-remove">
+      <button className="card__button card__button--remove" type="button" onClick={() => removeTodo(id)} data-testid="card-remove">
         <span className="sr-only">Remove</span>
         <FontAwesomeIcon icon={faTimes} />
       </button>
-      <button className="card__button" type="button" onClick={() => completeTodo(id)} data-testid="card-complete">
+      <button className="card__button card__button--complete" type="button" onClick={() => completeTodo(id)} data-testid="card-complete">
         <span className="sr-only">Complete</span>
         <FontAwesomeIcon icon={faCheck} />
       </button>
