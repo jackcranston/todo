131ffedb1215b82da847d9dd034f3a358223098b
Jack Cranston <jackcranston@itg.co.uk>
2021-04-21 17:35:37 +0100

    Added test coverage generation

--- src/components/card/index.test.js
+++ src/components/card/index.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, cleanup, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import { Provider } from 'react-redux';
